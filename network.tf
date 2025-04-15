8e6d21a8f1f99b1a0759f9d5d7dded37e462271e
EC2 Default User <ec2-user@ip-172-31-47-27.ec2.internal>
2025-04-15 01:52:59 +0000

    Corrigindo o checkov

--- network.tf
+++ network.tf
@@ -174,17 +174,17 @@ resource "aws_route_table_association" "public_assoc_1b" {
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.main.id
 
--  ingress {
--    protocol  = "-1"
--    self      = true
--    from_port = 0
--    to_port   = 0
--  }
+  ingress {
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
 
--  egress {
--    from_port   = 0
--    to_port     = 0
--    protocol    = "-1"
--    cidr_blocks = ["0.0.0.0/0"]
--  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
